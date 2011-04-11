9b2dbded99c2229515feec7dcd59036e90c16f60
Michael Moorman <tensorpuddin@devio.us>
2011-04-11 00:36:59 -0400

    finished init code for the moment

--- curator/backend.py
+++ curator/backend.py
@@ -39,16 +39,39 @@ class DBusService(dbus.service.Object):
         dbus.service.Object.__init__(self, bus_name, DBUS_PATH)
         # Initialize database connection
         self.db = db.Database('/home/michael/.local/share/curator/curator.db')
+        # Initialize cache
+        self.cache = set(self.db.select_all_paths())
+        # Start update looper
+        self.update_timer = gobject.timeout_add(10000, self.update_wallpapers)
         # Initialize queue
-        self.queue = Queue(self.db.select_all_nonrejects())
+        self.queue = Queue([])
+        self.__refresh_queue()
         # Get config from GConf
-        pass
+        self.gclient = gconf.client_get_default()
+        notify = self.gclient.get_bool(GCONF_NOTIFY_KEY)
+        if notify == None:
+            notify = True
+            self.gclient.set_bool(GCONF_NOTIFY_KEY, notify)
+        self.notify = notify
+        interval = self.gclient.get_int(GCONF_INTERVAL_KEY)
+        if interval == None:
+            interval = 30
+            self.gclient.set_int(GCONF_INTERVAL_KEY, interval)
+        self.interval = interval
+        # Start the wallpaper switcher
+        self.wallpaper_timer = gobject.timeout_add(60000*self.interval,
+                                                   self.next_wallpaper)
+        # Switch wallpaper for the first time
+        self.next_wallpaper()
 
-    def __del__(self):
-        pass
+    def __refresh_queue(self):
+        self.queue.append(self.db.select_all_nonrejects())
 
     def __set_wallpaper(self,key):
         name, path, rej = self.db.get_entry_by_key(key)
+        # If queue is empty, refresh it
+        if self.queue == Queue():
+            self.__refresh_queue()
         # set wallpaper here...
         if rej > 0:
             self.__set_wallpaper(self.queue.next)
@@ -60,6 +83,7 @@ class DBusService(dbus.service.Object):
                          in_signature = '', out_signature='')
     def next_wallpaper(self):
         self.__set_wallpaper(self.queue.next)
+        return True
 
     @dbus.service.method(DBUS_INTERFACE,
                          in_signature = '', out_signature='')
@@ -74,21 +98,23 @@ class DBusService(dbus.service.Object):
 
     @dbus.service.signal(DBUS_INTERFACE, signature = 's')
     def rejected_wallpaper(self,path):
-        pass
+        return path
 
     @dbus.service.signal(DBUS_INTERFACE, signature = 's')
     def unrejected_wallpaper(self,path):
-        pass
+        return path
 
     @dbus.service.signal(DBUS_INTERFACE, signature = 's')
     def changed_wallpaper(self,path):
-        pass
+        return path
 
     def update_wallpapers(self):
-        pass
+        self.cache = self.db.update(self.cache)
+        return True
 
 class DBusThread(threading.Thread):
     def run(self):
         DBusGMainLoop(set_as_default=True)
         object = DBusService()
-        gtk.main()
+        loop = gobject.MainLoop()
+        loop.run()
