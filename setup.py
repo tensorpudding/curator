4a40e812aa493cf4f7bea30e94a07254b7fa06d1
Michael Moorman <tensorpuddin@devio.us>
2011-04-15 01:16:10 -0400

    fixed comma

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
                    "for GNOME"),
     license = 'BSD License',
     scripts = ['scripts/curator'],
-    package_data = { 'curator': ['curator.ui'] }
+    package_data = { 'curator': ['curator.ui'] },
 )
