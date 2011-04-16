588883852f002ffba2b3ca425f7df5e209dcce3a
Michael Moorman <tensorpuddin@devio.us>
2011-04-15 20:14:13 -0400

    removed indicator config option

--- curator/watchdog.py
+++ curator/watchdog.py
@@ -4,7 +4,6 @@ GCONF_DIR = '/apps/curator'
 GCONF_NOTIFY_KEY = '/apps/curator/notifications'
 GCONF_INTERVAL_KEY = '/apps/curator/update_interval'
 GCONF_WALLPAPER_KEY = '/apps/curator/wallpaper_directory'
-GCONF_INDICATOR_KEY = '/apps/curator/use_indicator'
 
 class GConfWatchdog:
 
@@ -13,12 +12,12 @@ class GConfWatchdog:
         Begin listening on gconf, sending signals through the given D-Bus
         client.
         """
-        client = gconf.client_get_default()
-        client.add_dir(GCONF_DIR, gconf.CLIENT_PRELOAD_NONE)
+        self.client = gconf.client_get_default()
+        self.client.add_dir(GCONF_DIR, gconf.CLIENT_PRELOAD_ONELEVEL)
         self.dbus_client = dbus_client
-        client.notify_add(GCONF_NOTIFY_KEY, self.notify_callback)
-        client.notify_add(GCONF_INTERVAL_KEY, self.interval_callback)
-        client.notify_add(GCONF_WALLPAPER_KEY, self.wallpaper_callback)
+        self.client.notify_add(GCONF_NOTIFY_KEY, self.notify_callback)
+        self.client.notify_add(GCONF_INTERVAL_KEY, self.interval_callback)
+        self.client.notify_add(GCONF_WALLPAPER_KEY, self.wallpaper_callback)
 
     def get(self):
         """
@@ -32,13 +31,16 @@ class GConfWatchdog:
                  'wallpaper_directory': wallpaper_directory }
 
     def notify_callback(self, client, *args, **kwargs):
+        client = gconf.client_get_default()
         notify = client.get_bool(GCONF_NOTIFY_KEY)
         self.dbus_client.update_notifications(notify)
 
     def interval_callback(self, client, *args, **kwargs):
-        interval = gconf.get_int(GCONF_INTERVAL_KEY)        
+        client = gconf.client_get_default()
+        interval = client.get_int(GCONF_INTERVAL_KEY)        
         self.dbus_client.update_update_interval(interval)
 
     def wallpaper_callback(self, client, *args, **kwargs):
-        wallpaper_directory = gconf.get_string(GCONF_WALLPAPER_KEY)
+        client = gconf.client_get_default()
+        wallpaper_directory = client.get_string(GCONF_WALLPAPER_KEY)
         self.dbus_client.update_wallpaper_directory(wallpaper_directory)
