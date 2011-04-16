45c9f49ed0ad77538d463c48cb0dee6d16a35324
Michael Moorman <tensorpuddin@devio.us>
2011-04-16 11:28:18 -0400

    fixed thumbnail.generate

--- curator/thumbnail.py
+++ curator/thumbnail.py
@@ -7,11 +7,17 @@ class ThumbnailGenerator:
 
         self.geom = geom
 
-    def generate(self, image_path, thumbnail_path):
-        im = Image.open(image_path)
-        im.thumbnail(self.geom)
-        base, ext = os.path.splitext(path)
-        im.save(thumbnail_path, imghdr.what(image_path))
+    def generate(self, image_path):
+        """
+        Given the image, produces a thumbnail, saves with a random name in the
+        thumbnail directory, and returns the name
+        """
+        return 'foo'
+
+#         im = Image.open(image_path)
+#         im.thumbnail(self.geom)
+#         base, ext = os.path.splitext(path)
+#         im.save(thumbnail_path, imghdr.what(image_path))
 
     def update(self, image_path, thumbnail_path):
         pass
