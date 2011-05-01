f3236309834ddb8758d52bc2d731350762256b94
Michael Moorman <tensorpuddin@devio.us>
2011-05-01 17:39:11 -0400

    adding icons for all sizes

--- setup.py
+++ setup.py
@@ -13,6 +13,30 @@ class install(_install):
         subprocess.call(["gconftool-2", "--install-schema-file",
                          "share/curator.schemas"])
 
+_data_files = [
+    ('share/icons/hicolor/scalable/apps', ['share/icons/hicolor/scalable/apps/curator.svg']),
+    ('share/icons/hicolor/256x256/apps', ['share/icons/hicolor/256x256/apps/curator.png']),
+    ('share/icons/hicolor/192x192/apps', ['share/icons/hicolor/192x192/apps/curator.png']),
+    ('share/icons/hicolor/128x128/apps', ['share/icons/hicolor/128x128/apps/curator.png']),
+    ('share/icons/hicolor/96x96/apps', ['share/icons/hicolor/96x96/apps/curator.png']),
+    ('share/icons/hicolor/72x72/apps', ['share/icons/hicolor/72x72/apps/curator.png']),
+    ('share/icons/hicolor/64x64/apps', ['share/icons/hicolor/64x64/apps/curator.png']),
+    ('share/icons/hicolor/48x48/apps', ['share/icons/hicolor/48x48/apps/curator.png']),
+    ('share/icons/hicolor/36x36/apps', ['share/icons/hicolor/36x36/apps/curator.png']),
+    ('share/icons/hicolor/32x32/apps', ['share/icons/hicolor/32x32/apps/curator.png']),
+    ('share/icons/hicolor/24x24/apps', ['share/icons/hicolor/24x24/apps/curator.png']),
+    ('share/icons/hicolor/22x22/apps', ['share/icons/hicolor/22x22/apps/curator.png']),
+    ('share/icons/hicolor/16x16/apps', ['share/icons/hicolor/16x16/apps/curator.png']),
+    ('share/icons/ubuntu-mono-dark/apps/16x16', ['share/icons/ubuntu-mono-dark/apps/16x16/curator.svg']),
+    ('share/icons/ubuntu-mono-dark/apps/22x22', ['share/icons/ubuntu-mono-dark/apps/22x22/curator.svg']),
+    ('share/icons/ubuntu-mono-dark/apps/24x24', ['share/icons/ubuntu-mono-dark/apps/24x24/curator.svg']),
+    ('share/icons/ubuntu-mono-dark/apps/48x48', ['share/icons/ubuntu-mono-dark/apps/48x48/curator.svg']),
+    ('share/icons/ubuntu-mono-light/apps/16x16', ['share/icons/ubuntu-mono-light/apps/16x16/curator.svg']),
+    ('share/icons/ubuntu-mono-light/apps/22x22', ['share/icons/ubuntu-mono-light/apps/22x22/curator.svg']),
+    ('share/icons/ubuntu-mono-light/apps/24x24', ['share/icons/ubuntu-mono-light/apps/24x24/curator.svg']),
+    ('share/icons/ubuntu-mono-light/apps/48x48', ['share/icons/ubuntu-mono-light/apps/48x48/curator.svg']),
+    ('share/applications', ['share/applications/curator.desktop']),
+    ]
 setup(
     name = 'curator',
     version = '0.0.3',
