00635b212c5748a7cd91b86206977cc7a1716e54
Michael Moorman <tensorpuddin@devio.us>
2011-05-02 19:30:33 -0400

    pixmaps

--- make_icons.sh
+++ make_icons.sh
@@ -5,8 +5,11 @@ rm -rf share/icons
 dir=share/icons/hicolor/scalable/apps
 mkdir -p ${dir}
 cp share/curator.svg ${dir}/curator.svg
+rsvg-convert -w 48 -h 48 -o share/pixmaps/curator.png share/curator.svg
+rsvg-convert -w 32 -h 32 -o share/tmp.png share/curator.svg
+convert share/tmp.png share/pixmaps/curator.xpm
+rm share/tmp.png
 for size in 16 22 24 32 36 48 64 72 96 128 192 256; do
-
     dir=share/icons/hicolor/${size}x${size}/apps
     mkdir -p ${dir}
     rsvg-convert -w ${size} -h ${size} -o ${dir}/curator.png share/curator.svg
